28ec96e1a1368b919f519e3e38cceac57c981689
Alexander Kiseliev <i43ack@gmail.com>
2017-04-17 16:08:28 +0300

    blocks order

--- lib/hancock/pages/models/mongoid/blockset.rb
+++ lib/hancock/pages/models/mongoid/blockset.rb
@@ -9,7 +9,7 @@ module Hancock::Pages
 
           field :name, type: String, default: "", overwrite: true
 
-          embeds_many :blocks, inverse_of: :blockset, class_name: "Hancock::Pages::Block"
+          embeds_many :blocks, inverse_of: :blockset, class_name: "Hancock::Pages::Block", order: [:order, :asc]
           accepts_nested_attributes_for :blocks, allow_destroy: true
 
           field :use_wrapper, type: Boolean, default: false
@@ -18,7 +18,7 @@ module Hancock::Pages
           field :wrapper_id, type: String, default: ""
           field :wrapper_attributes, type: Hash, default: {}
         end
-        
+
       end
     end
   end
